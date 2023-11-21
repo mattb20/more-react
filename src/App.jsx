8ae8e30b624d1deaa5e477c74fba1213e4ee9459
mattb20 <matt_burgoyne@live.co.uk>
2023-11-21 12:16:24 +0000

    add Player component add gitignore file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
-function App() {
-  
+import Player from './components/Player.jsx';
 
+function App() {  
   return (
     <main>
       <div id="game-container">
         ## Players component
+        <ol id="players">
+          <Player name="Player 1" symbol="X"/>
+          <Player name="Player 2" symbol="O"/>
+        </ol>
         ## Game board component
       </div>
       ## Game log component
