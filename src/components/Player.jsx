405e33bfe3b79e85ff4cfc0af2ab59967e5d951d
mattb20 <matt_burgoyne@live.co.uk>
2023-11-21 14:15:11 +0000

    use function reference inside onClick

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,29 @@
-export default function Player({name, symbol}) {
-    return (
-        <li>
-            <span className="player">
-              <span className='player-name'>{name}</span>
-              <span className='player-symbol'>{symbol}</span>
-              <button>Edit</button>
-            </span>
-          </li>
-    );
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+  const [isEditing, setIsEditing] = useState(false);
+  console.log(isEditing);
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input
+            onKeyDown={(key) => {
+              console.log(key);
+              if (key.code === "Enter") {
+                setIsEditing(false);
+              }
+            }}
+          ></input>
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={handleEditClick}>Edit</button>
+      </span>
+    </li>
+  );
 }
